af3e0904a13cdf19d6f1a892d88896bb7f04fde1
Mr-Dutchman <meelisfidelis@gmail.com>
2021-07-13 17:12:44 +0100

    motherfucker

--- client/signup.js
+++ client/signup.js
@@ -61,7 +61,7 @@ document.addEventListener('submit', function(e) {
     //console.log(userData)
 
     document.addEventListener('submit', function(){
-        fetch('http://localhost:8000/insert', option).then(response =>{
+        fetch('http://localhost:3000/insert', option).then(response =>{
             console.log(response.body.getReader())
         })
 
